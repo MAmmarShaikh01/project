0f8d1311785e287097dbd68c88b541629afa1ec4
MAmmarShaikh01 <shaikhurwa61@gmail.com>
2024-11-09 15:04:40 +0500

    contact

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -1,28 +1,13 @@
-import Link from 'next/link';
-import styles from '../../css/Header.module.css';
+import Header from "../../components/Header";
+import Footer from "../../components/footer";
+import ContactPage from "../../components/ContactPage";
 
-const Header: React.FC = () => {
+export default function Home() {
   return (
-    <header className={styles.header}>
-      <div className={styles.container}>
-        <h1 className={styles.logo}>
-          <Link href="/">My Portfolio</Link>
-        </h1>
-
-        {/* Hidden checkbox for mobile menu toggle */}
-        <input type="checkbox" id="menuToggle" className={styles.menuCheckbox} />
-        <label htmlFor="menuToggle" className={styles.menuButton} aria-label="Toggle menu">
-          ☰
-        </label>
-
-        <nav className={styles.nav}>
-          <Link href="/" className={styles.navLink}>Home</Link>
-          <Link href="/about" className={styles.navLink}>About</Link>
-          <Link href="/contact" className={styles.navLink}>Contact</Link>
-        </nav>
-      </div>
-    </header>
+    <>
+    <Header />
+    <ContactPage />
+    <Footer />
+    </>
   );
-};
-
-export default Header;
+}
