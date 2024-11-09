5ccf0d8cc5b463c4ca7e417be958824c1fa2e052
MAmmarShaikh01 <shaikhurwa61@gmail.com>
2024-11-09 09:33:49 +0500

    new

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,6 @@
 import Image from "next/image";
 import Header from "../components/Header";
-import Footer from "@/components/footer";
+import Footer from "../components/footer";
 export default function Home() {
   return (
     <>
