e59e19444e2b0c4b6fb8bd543d8a20658cd53021
MAmmarShaikh01 <shaikhurwa61@gmail.com>
2024-11-09 13:58:08 +0500

    my-appfe

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,6 @@
 import Header from "../components/Header";
 import Footer from "../components/footer";
-import MainContent from "../components/MainContent";
+import MainContent from "../components/About";
 
 export default function Home() {
   return (
